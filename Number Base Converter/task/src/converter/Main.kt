f142c4621acd4f6f4cc99dc68e5f2fe456492049
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-03-31 20:34:28 +0300

    Added solution for stage 1

--- Number Base Converter/task/src/converter/Main.kt
+++ Number Base Converter/task/src/converter/Main.kt
@@ -3,5 +3,8 @@ package converter
 fun main() {
     println("Enter number in decimal system:")
     val number = readLine()!!.toLong()
-
+    println("Enter target base:")
+    val radix = readLine()!!.toInt()
+    val result = number.toString(radix)
+    println("Conversion result: $result")
 }
