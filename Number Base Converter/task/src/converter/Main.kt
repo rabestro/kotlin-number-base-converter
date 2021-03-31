5d0f7db2eaa1ce59b1f38bef681bf22b5f5bc345
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-03-31 21:20:25 +0300

    Solution for stage 2

--- Number Base Converter/task/src/converter/Main.kt
+++ Number Base Converter/task/src/converter/Main.kt
@@ -1,10 +1,30 @@
 package converter
 
 fun main() {
+    while (true) {
+        println("Do you want to convert /from decimal or /to decimal? (To quit type /exit)")
+        when (readLine()!!){
+            "/from" -> from()
+            "/to" -> to()
+            "/exit"-> break
+        }
+    }
+}
+
+fun from() {
     println("Enter number in decimal system:")
     val number = readLine()!!.toLong()
     println("Enter target base:")
     val radix = readLine()!!.toInt()
     val result = number.toString(radix)
     println("Conversion result: $result")
+}
+
+fun to() {
+    println("Enter source number:")
+    val number = readLine()!!
+    println("Enter source base:")
+    val radix = readLine()!!.toInt()
+    val result = number.toLong(radix)
+    println("Conversion to decimal result: $result")
 }
