1c26cd38013b2953765056993187ef5215091aad
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-04-03 15:10:27 +0300

    Completed tests for stage 4

--- Number Base Converter/task/test/ConverterTest.java
+++ Number Base Converter/task/test/ConverterTest.java
@@ -6,8 +6,6 @@ import org.hyperskill.hstest.testing.TestedProgram;
 
 import java.math.BigInteger;
 import java.text.MessageFormat;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Random;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -18,53 +16,92 @@ public class ConverterTest extends StageTest {
     private static final int MAX_SOURCE_FRACTION_LENGTH = 10;
     private static final int MAX_TARGET_FRACTION_LENGTH = 5;
     private static final String DIGITS = "0123456789abcdefghijklmnopqrstuvwxyz";
+    private static final int MINIMUM_BASE = 2;
+    private static final int MAXIMUM_BASE = DIGITS.length();
+    private static final int MINIMUM_TESTS = 1;
+    private static final int MAXIMUM_TESTS = 10;
+    private static final Pattern DOT_PATTERN = Pattern.compile(".", Pattern.LITERAL);
     private static final Pattern RESULT_PATTERN = Pattern.compile(
             "Conversion result:\\s*(?<number>.*?)\\s*", Pattern.CASE_INSENSITIVE);
 
-    @DynamicTest(order = 10)
-    CheckResult testFirstMessageAndExit() {
-        final var main = new TestedProgram();
-        var output = main.start().toLowerCase();
+    private static int getRandomBase() {
+        return MINIMUM_BASE + random.nextInt(MAXIMUM_BASE - MINIMUM_BASE);
+    }
 
+    private static String generateSourceNumber(final int radix) {
+        final var sourceWhole = random
+                .ints(1 + random.nextInt(MAX_SOURCE_WHOLE_LENGTH), 0, radix)
+                .map(DIGITS::charAt)
+                .mapToObj(Character::toString)
+                .collect(Collectors.joining());
+
+        if (random.nextBoolean()) {
+            return sourceWhole;
+        }
+
+        final var sourceFraction = random
+                .ints(1 + random.nextInt(MAX_SOURCE_FRACTION_LENGTH), 0, radix)
+                .map(DIGITS::charAt)
+                .mapToObj(Character::toString)
+                .collect(Collectors.joining());
+
+        return sourceWhole + '.' + sourceFraction;
+    }
+
+    private static String convert(String sourceNumber, int sourceBase, int targetBase) {
+        if (!sourceNumber.contains(".")) {
+            return new BigInteger(sourceNumber, sourceBase).toString(targetBase);
+        }
+        final var numberParts = DOT_PATTERN.split(sourceNumber);
+        final var targetWhole = new BigInteger(numberParts[0], sourceBase).toString(targetBase);
+        final var sourceFraction = numberParts[1].chars().map(DIGITS::indexOf).toArray();
+
+        var fraction = 0.0;
+        var divider = (double) sourceBase;
+        for (final int digit : sourceFraction) {
+            fraction += digit / divider;
+            divider *= sourceBase;
+        }
+        var targetFraction = new StringBuilder();
+        for (int i = 0; i < MAX_TARGET_FRACTION_LENGTH; ++i) {
+            fraction *= targetBase;
+            final int index = (int) fraction;
+            targetFraction.append(DIGITS.charAt(index));
+            fraction -= index;
+        }
+        return targetWhole + '.' + targetFraction.toString();
+    }
+
+    private static void require(final boolean condition, final String error, final Object... args) {
+        if (!condition) {
+            final var feedback = MessageFormat.format(error, args);
+            throw new WrongAnswer(feedback);
+        }
+    }
+
+    @DynamicTest()
+    CheckResult testOutputFormat() {
+        final var main = new TestedProgram();
+        final var output = main.start().toLowerCase();
         require(output.contains("source base")
                 && output.contains("target base") && output.contains("/exit"), "" +
                 "Your program should output the message \"Enter two numbers in format: " +
                 "{source base} {target base} (To quit type /exit)\" when it starts");
+
+        int testCases = 20;
+        while (testCases-- > 0) {
+            testConversion(main, getRandomBase(), getRandomBase());
+        }
 
         main.execute("/exit");
         require(main.isFinished(), "Your program should terminate when the user enters \"/exit\"");
         return CheckResult.correct();
     }
 
-    static class TestCase {
-        final int sourceBase;
-        final int targetBase;
-        final List<String[]> cases;
-
-        TestCase(int from, int to, String[][] testCases) {
-            sourceBase = from;
-            targetBase = to;
-            cases = Arrays.asList(testCases);
-        }
-    }
-
-    private final TestCase[] testCases = new TestCase[]{
-            new TestCase(10, 2, new String[][]{
-                    {"12345", "11000000111001"}
-            })
-
-    };
-
-    @DynamicTest(order = 20, data = "testCases")
-    CheckResult testOutputFormat(final TestCase test) {
-        final var sourceBase = test.sourceBase;
-        final var targetBase = test.targetBase;
-
-        final var main = new TestedProgram();
-        main.start();
+    CheckResult testConversion(TestedProgram main, int sourceBase, int targetBase) {
         var output = main.execute(sourceBase + " " + targetBase).toLowerCase();
-
-        for (var data : test.cases) {
+        int count = MINIMUM_TESTS + random.nextInt(MAXIMUM_TESTS - MINIMUM_TESTS + 1);
+        while (count-- > 0) {
             require(output.contains("base " + sourceBase)
                     && output.contains("convert to base " + targetBase), "" +
                     "Your program should prompt the user for the number to be " +
@@ -79,8 +116,7 @@ public class ConverterTest extends StageTest {
                     "{0} to convert to base {1} " +
                     "(To go back type /back)\"", sourceBase, targetBase);
 
-            final var sourceNumber = data[0];
-            final var expectedResult = data[1];
+            final var sourceNumber = generateSourceNumber(sourceBase);
             output = main.execute(sourceNumber);
             final var lines = output.lines().toArray(String[]::new);
 
@@ -94,6 +130,7 @@ public class ConverterTest extends StageTest {
                     "Your program should print the conversion result in the " +
                     "format \"Conversion result: CONVERTED_NUMBER\"");
 
+            final var expectedResult = convert(sourceNumber, sourceBase, targetBase);
             final var actualResult = result.group("number");
 
             require(!actualResult.isBlank(), "You program didn't print the converted number");
@@ -118,64 +155,6 @@ public class ConverterTest extends StageTest {
                 "Your program should take the user back to the top-level " +
                 "menu when they enter \"/back\"");
 
-        main.execute("/exit");
-        require(main.isFinished(), "Your program should terminate when the user enters \"/exit\"");
         return CheckResult.correct();
-    }
-
-    CheckResult testConversion(int sourceBase, int targetBase) {
-
-        return CheckResult.correct();
-    }
-
-    private static String generateSourceNumber(final int radix) {
-        final var sourceWhole = random
-                .ints(random.nextLong() % MAX_SOURCE_WHOLE_LENGTH, 0, radix)
-                .map(DIGITS::charAt)
-                .mapToObj(Character::toString)
-                .collect(Collectors.joining());
-
-        if (random.nextBoolean()) {
-            return sourceWhole;
-        }
-
-        final var sourceFraction = random
-                .ints(random.nextLong() % MAX_SOURCE_FRACTION_LENGTH, 0, radix)
-                .map(DIGITS::charAt)
-                .mapToObj(Character::toString)
-                .collect(Collectors.joining());
-
-        return sourceWhole + '.' + sourceFraction;
-    }
-
-    private static String convert(String sourceNumber, int sourceBase, int targetBase) {
-        if (!sourceNumber.contains(".")) {
-            return new BigInteger(sourceNumber, sourceBase).toString(targetBase);
-        }
-        final var numberParts = sourceNumber.split(".");
-        final var targetWhole = new BigInteger(numberParts[0], sourceBase).toString(targetBase);
-        final var sourceFraction = numberParts[1].chars().map(DIGITS::indexOf).toArray();
-
-        var fraction = 0.0;
-        var divider = (double) sourceBase;
-        for (final int digit : sourceFraction) {
-            fraction += digit / divider;
-            divider *= sourceBase;
-        }
-        var targetFraction = "";
-        for (int i = 0; i < MAX_TARGET_FRACTION_LENGTH; ++i) {
-            fraction *= targetBase;
-            int index = (int) fraction;
-            targetFraction += DIGITS.charAt(index);
-            fraction -= index;
-        }
-        return targetWhole + '.' + targetFraction;
-    }
-
-    private static void require(final boolean condition, final String error, final Object... args) {
-        if (!condition) {
-            final var feedback = MessageFormat.format(error, args);
-            throw new WrongAnswer(feedback);
-        }
     }
 }
