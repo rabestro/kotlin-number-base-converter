c6f5ef7317798830e08f5c31561c32d77883439b
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-04-04 21:47:01 +0300

    Updated tests for stage 4

--- Number Base Converter/task/test/NumeralSystemConverterTest.java
+++ Number Base Converter/task/test/NumeralSystemConverterTest.java
@@ -3,91 +3,27 @@ import org.hyperskill.hstest.exception.outcomes.WrongAnswer;
 import org.hyperskill.hstest.stage.StageTest;
 import org.hyperskill.hstest.testcase.CheckResult;
 import org.hyperskill.hstest.testing.TestedProgram;
+import utils.Numbers;
 
 import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.math.RoundingMode;
+import java.math.MathContext;
 import java.text.MessageFormat;
-import java.util.Random;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static java.math.MathContext.DECIMAL32;
+import static utils.Numbers.*;
 
 public class NumeralSystemConverterTest extends StageTest {
-    private static final Random random = new Random();
-    private static final int MAX_SOURCE_WHOLE_LENGTH = 20;
-    private static final int MAX_SOURCE_FRACTION_LENGTH = 10;
-    private static final int MAX_TARGET_FRACTION_LENGTH = 5;
-    private static final int MAX_FRACTION_COMPARE = MAX_TARGET_FRACTION_LENGTH - 1;
-    private static final int SCALE = 10;
-    private static final String DIGITS = "0123456789abcdefghijklmnopqrstuvwxyz";
-    private static final int MINIMUM_BASE = 2;
-    private static final int MAXIMUM_BASE = DIGITS.length();
-    private static final int MINIMUM_TESTS = 1;
-    private static final int MAXIMUM_TESTS = 10;
+    private static final int CONVERSION_TESTS_NUMBER = 10;
+    private static final int BASE_TESTS_NUMBER = 30;
+    private static final BigDecimal DELTA = new BigDecimal("0.00001", new MathContext(5));
+
     private static final Pattern DOT_PATTERN = Pattern.compile(".", Pattern.LITERAL);
     private static final Pattern RESULT_PATTERN = Pattern.compile(
             "Conversion result:\\s*(?<number>.*?)\\s*", Pattern.CASE_INSENSITIVE);
 
-    private static int getRandomBase() {
-        return MINIMUM_BASE + random.nextInt(MAXIMUM_BASE - MINIMUM_BASE);
-    }
-
-    private static String generateSourceNumber(final int radix) {
-        final var sourceWhole = random
-                .ints(1 + random.nextInt(MAX_SOURCE_WHOLE_LENGTH), 0, radix)
-                .map(DIGITS::charAt)
-                .mapToObj(Character::toString)
-                .collect(Collectors.joining());
-
-        if (random.nextBoolean()) {
-            return sourceWhole;
-        }
-
-        final var sourceFraction = random
-                .ints(1 + random.nextInt(MAX_SOURCE_FRACTION_LENGTH), 0, radix)
-                .map(DIGITS::charAt)
-                .mapToObj(Character::toString)
-                .collect(Collectors.joining());
-
-        return sourceWhole + '.' + sourceFraction;
-    }
-
-    private static String convert(String sourceNumber, int sourceBase, int targetBase) {
-        if (!sourceNumber.contains(".")) {
-            return sourceBase == targetBase
-                    ? sourceNumber
-                    : new BigInteger(sourceNumber, sourceBase).toString(targetBase);
-        }
-        final var numberParts = DOT_PATTERN.split(sourceNumber);
-        if (sourceBase == targetBase) {
-            return numberParts[0] + '.' + (numberParts[1] + "00000").substring(0, 5);
-        }
-        final var targetWhole = new BigInteger(numberParts[0], sourceBase).toString(targetBase);
-        final var sourceFraction = numberParts[1].chars().map(DIGITS::indexOf).toArray();
-
-        var fraction = BigDecimal.ZERO.setScale(SCALE, RoundingMode.HALF_EVEN);
-        var divider = new BigDecimal(sourceBase).setScale(SCALE, RoundingMode.HALF_EVEN);
-        var bigSourceBase = new BigDecimal(sourceBase).setScale(SCALE, RoundingMode.HALF_EVEN);
-        var bigTargetBase = new BigDecimal(targetBase).setScale(SCALE, RoundingMode.HALF_EVEN);
-
-        for (final int digit : sourceFraction) {
-            var delta = new BigDecimal(digit)
-                    .setScale(SCALE, RoundingMode.HALF_EVEN)
-                    .divide(divider, RoundingMode.HALF_EVEN);
-            fraction = fraction.add(delta);
-            divider = divider.multiply(bigSourceBase);
-        }
-        var targetFraction = new StringBuilder();
-        for (int i = 0; i < MAX_TARGET_FRACTION_LENGTH * 2; ++i) {
-            fraction = fraction.multiply(bigTargetBase);
-            final int index = fraction.intValue();
-            targetFraction.append(DIGITS.charAt(index));
-            fraction = fraction.subtract(new BigDecimal(index));
-        }
-        return targetWhole + '.' + targetFraction.substring(0, MAX_TARGET_FRACTION_LENGTH);
-    }
-
-    private static void require(final boolean condition, final String error, final Object... args) {
+    static void require(final boolean condition, final String error, final Object... args) {
         if (!condition) {
             final var feedback = MessageFormat.format(error, args);
             throw new WrongAnswer(feedback);
@@ -103,20 +39,19 @@ public class NumeralSystemConverterTest extends StageTest {
                 "Your program should output the message \"Enter two numbers in format: " +
                 "{source base} {target base} (To quit type /exit)\" when it starts");
 
-        int testCases = 20;
-        while (testCases-- > 0) {
-            testConversion(main, getRandomBase(), getRandomBase());
-        }
+        IntStream.range(0, BASE_TESTS_NUMBER).forEach(i -> testConversion(main));
 
         main.execute("/exit");
         require(main.isFinished(), "Your program should terminate when the user enters \"/exit\"");
         return CheckResult.correct();
     }
 
-    private void testConversion(TestedProgram main, int sourceBase, int targetBase) {
+    private void testConversion(final TestedProgram main) {
+        final int sourceBase = getRandomBase();
+        final int targetBase = getRandomBase();
         var output = main.execute(sourceBase + " " + targetBase).toLowerCase();
-        int count = MINIMUM_TESTS + random.nextInt(MAXIMUM_TESTS - MINIMUM_TESTS + 1);
-        while (count-- > 0) {
+        int testCount = CONVERSION_TESTS_NUMBER;
+        while (testCount-- > 0) {
             require(output.contains("base " + sourceBase)
                     && output.contains("convert to base " + targetBase), "" +
                     "Your program should prompt the user for the number to be " +
@@ -131,7 +66,7 @@ public class NumeralSystemConverterTest extends StageTest {
                     "{0} to convert to base {1} " +
                     "(To go back type /back)\"", sourceBase, targetBase);
 
-            final var sourceNumber = generateSourceNumber(sourceBase);
+            final var sourceNumber = Numbers.generateNumber(sourceBase);
             output = main.execute(sourceNumber);
             final var lines = output.lines().toArray(String[]::new);
 
@@ -150,7 +85,7 @@ public class NumeralSystemConverterTest extends StageTest {
 
             require(!actualResult.isBlank(), "You program didn't print the converted number");
 
-            requireEqual(expectedResult, actualResult);
+            requireEqual(expectedResult, actualResult, targetBase);
 
             require(!main.isFinished(), "" +
                     "Your program should not terminate until the user enter " +
@@ -167,7 +102,7 @@ public class NumeralSystemConverterTest extends StageTest {
                 "menu when they enter \"/back\"");
     }
 
-    private void requireEqual(final String expectedNumber, final String actualNumber) {
+    private void requireEqual(final String expectedNumber, final String actualNumber, final int radix) {
         final var expected = DOT_PATTERN.split(expectedNumber);
         final var actual = DOT_PATTERN.split(actualNumber);
 
@@ -200,14 +135,21 @@ public class NumeralSystemConverterTest extends StageTest {
                         "be 5 digits in length. Expected fractional part: {0} Actual fractional part: {1}",
                 expectedFraction, actualFraction);
 
-        require(expectedFraction.substring(0, MAX_FRACTION_COMPARE).equalsIgnoreCase(
-                actualFraction.substring(0, MAX_FRACTION_COMPARE)), "" +
-                "The actual result {0} is not equal to expected {1}. " +
-                "A whole part of the numbers is equal, but the fractional part is different." +
-                "The first {2} digits of fractional part doesn''t equals to expected digits." +
-                " The expected factional part is {3} but your actual fractional part is {4}.",
-                expectedNumber, actualNumber, MAX_FRACTION_COMPARE, expectedFraction, actualFraction);
+        if (actualFraction.equalsIgnoreCase(expectedFraction)) {
+            return;
+        }
+        final var actualDecimalFraction = convertFractionToDecimal(actualFraction, radix);
+        final var expectedDecimalFraction = convertFractionToDecimal(expectedFraction, radix);
+        final var difference = expectedDecimalFraction.subtract(actualDecimalFraction, DECIMAL32).abs();
+
+        require(difference.compareTo(DELTA) < 0, "" +
+                        "The actual result {1} is not equal to expected {0}. " +
+                        "A whole part of the numbers is equal, but the fractional part is different. " +
+                        "The decimal difference of fractional parts is {2} and this is bigger then delta {3}. " +
+                        "Expected decimal fraction: {4} actual: {5}. ",
+                expectedNumber, actualNumber,
+                difference.toString(), DELTA.toString(),
+                expectedDecimalFraction.toString(),
+                actualDecimalFraction.toString());
     }
 }
-
-
